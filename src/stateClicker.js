13fd656e1c5f46ca63c1ab0d7d771e5fb184b241
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-05-14 15:03:31 +0200

    created Component and wrote logic

---
+++ src/stateClicker.js
@@ -0,0 +1,30 @@
+import React, {
+    Component
+} from 'react'
+
+class StateClicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {num: 0, winNum: 7, won: false}
+        this.randNum = this.randNum.bind(this)
+    }
+
+    randNum(e){
+        let num = Math.floor(Math.random() * 11);
+        this.setState({num: num});
+        if(num === this.state.winNum){
+            this.setState({won: true});
+        }
+    }
+
+    render(){
+        return(
+            <div className="StateClicker">
+                <h1>Your number is: {this.state.num}</h1>
+                {this.state.won? <h2>YOU WON!</h2> : <button onClick={this.randNum}>Click me!</button>}
+            </div>
+        )
+    }
+}
+
+export default StateClicker;
